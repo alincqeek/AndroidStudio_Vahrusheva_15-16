41d53a6033950af4b31296d695989b6287e00c91
alincqeek <a72658695@gmail.com>
2026-03-04 21:46:47 +0300

    Step 1: Add Navigation Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
